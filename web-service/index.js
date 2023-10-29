b43ca20ec7c7656f69613db86e79814bdda2fba6
Lobanov Ivan <physic2003@mail.ru>
2023-10-29 13:37:32 +0300

    add post-handler in backend/app.js and station emitter data

--- web-service/index.js
+++ web-service/index.js
@@ -1,45 +1,106 @@
-const express = require('express'); // импорт библиотеки express
-const path = require('path'); // импорт библиотеки path для работы с путями
-const app = express(); // создание экземпляра приложения express
+const express = require('express'); // Исправленный импорт библиотеки Express
+const axios = require('axios'); // для асинхронных запросов 
+const app = express(); // Создание экземпляра приложения Express
 const PORT = process.env.PORT || 3000; // присвоения порта
+const targetUrl = "127.0.0.1:3001/station_charge"
+const interval = 500; // миллисекунды 
 
+app.use(express.json()); // middleware для более простого парсинга тела запроса post 
 
-// структура данных о марсоходе 
+// Создаем объект, который будет содержать направление и значение направления для скорости 
+const DirecionEnumX = {
+  0: -1, // означает, что по направлению 0 (вправо), мы будем уменьшать нашу координату
+  1: -1,
+  2: 0, // означает, что по направлению 2 (вниз), мы ничего не будем делать с абсциссой точки 
+  3: 1,
+  4: 1,
+  5: 1,
+  6: 0,
+  7: -1, 
+  8: 0
+};
+
+// Создаем объект, который будет содержать направление и значение направления для скорости 
+const DirecionEnumY = {
+  0: 0,
+  1: -1,
+  2: -1,
+  3: -1,
+  4: 0,
+  5: 1,
+  6: 1,
+  7: 1,
+  8: 0
+};
+
+// структура данных марсохода
 class Stations {
-  update (data) {
-    this.id = data.id;
-    this.name = data.name;
+  constructor(name, x, y, v, direction, charge) {
+    this.id = 1;
+    this.name = name;
+    this.x = x;
+    this.y = y;
+    this.v = v;           
+    this.direction = direction; // 0 - это вправо, 2 - вниз, 4 - влево, 6 - вверх // всего 8 направлений 
+    this.charge = charge;       // абсцисса у нас растет влево, а ордината вверх
+  }                             // состояние 8 - остановка
+
+  update(data) {
     this.x = data.x;
     this.y = data.y;
     this.v = data.v;
+    this.direction = data.direction;
     this.charge = data.charge;
   }
 }
 
-stat = new Stations(); // наш марсоход 
-
-app.use(express.json()); // middleware для более простого парсинга тела запроса post 
+stat = new Stations('R2D2', 0, 0, 5, 8, 100); // наш марсоход 
 
 app.post('/update', (req, res) => {
   const data = req.body;
-  stat.update(data);
-})
-
-
-// описание long polling запроса
-app.get('/status', (req, res) => {
-  if (stat.charge != 0) {
-    res.json(stat);
+  if (data) {
+    stat.update(data);
+    res.status(200).json({status: "OK"});
   } else {
-    res.send("server in not responding....");
+    res.status(400).json({error: "Request body is empty"});
   }
-})
+});
 
-app.get('/charge', (req, res) => {
-  res.send(stat.charge);
-})
+// app.get('/status', (req, res) => {
+//   if (stat.charge !== 0) {
+//     res.status(200).json(stat);
+//   } else {
+//     res.status(500).json({error: "Server iSs not responding...."});
+//   }`
+// });
 
 
+function DataAboutStation() {
+  const sign_Vx = DirecionEnumX[stat.direction] || 0; // Получаем значение из словаря или 0, если значения нет
+  const sign_Vy = DirecionEnumY[stat.direction] || 0;
+
+  // Умножаем скорость на значение из словарей
+  stat.x += sign_Vx * stat.v;
+  stat.y += sign_Vy * stat.v;
+  
+  axios.post(targetUrl, {
+    id: stat.id,
+    name: stat.name,
+    x: stat.x,
+    y: stat.y,
+    v: stat.v,
+    direction: stat.direction,
+    charge: stat.charge
+   })
+    .then(response => {
+      console.log('Запрос успешно отправлен:');
+    })
+    .catch(error => {
+      console.error('Ошибка при отправке запроса:', error);
+    });
+}
+
+setInterval(DataAboutStation, interval);
 
 // запуск сервера приложения
 app.listen(PORT, () => {
